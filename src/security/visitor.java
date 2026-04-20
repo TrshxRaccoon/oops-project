323fbd4e2fdabd29ea06323da3660b7a178d2f87
MnnSucksAtThis <manana7900@gmail.com>
2026-04-20 22:53:47 +0530

    Apparently the method style we use is called JavaBeans and Java was named a coffeebean lmao

--- src/security/visitor.java
+++ src/security/visitor.java
@@ -1,34 +1,30 @@
 package security;
 
 import users.User;
-import java.time.LocalDateTime;
 
 public class visitor extends user
 {
     protected String ID; //visitors ID
-    protected boolean authorisedToEnter;
-    public static class logs
+    protected String name; 
+    protected String visiteeId; //Id of resident being visited
+    protected boolean isGuardian;
+    protected boolean isAuthorised;
+
+    public visitor(String ID , String name , String visiteeId , boolean g , boolean auth)
     {
-        protected String dateAndTime;
-        protected String visiteeId; //Id of resident being visited
-        protected String type; // Entry or exit or unauth
-        protected boolean authorised; 
+        this.ID = ID;
+        this.name = name;
+        this.visiteeId = visiteeId;
+        this.isGuardian = g ;
+        this.isAuthorised = auth;
+    }
 
-        public logs() 
-        {
-            LocalDateTime now = LocalTimeDate.now()
-            this.dateAndTime = String.toString(now);
-            this.visiteeId = null;
-            this.authorised = false;
-            this.visiteeId = null;
-        }
-
-        public logs(String dateAndTime , String visiteeId , String type) 
-        {
-            this.dateAndTime = dateAndTime;
-            this.visiteeId = null;
-            this.authorised = false;
-            this.visiteeId = null;
-        }
+    public visitor(String ID , String name , String visiteeId , boolean g)
+    {
+        this.ID = ID ;
+        this.name = name ;
+        this.visiteeId = visiteeId ;
+        this.isGuardian = g ;
+        this.isAuthorised = g;
     }
 }
