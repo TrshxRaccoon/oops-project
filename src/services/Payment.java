ca08bc1a38b84e4059d46d2e4d76c4eb9c36379e
unnatagarwal <unnatagarwal.work@gmail.com>
2026-04-21 23:58:29 +0530

    updates fees

--- src/services/Payment.java
+++ src/services/Payment.java
@@ -1,8 +1,8 @@
 package services;
-
 import java.time.LocalDateTime;
 
-public class Payment {
+public class Payment 
+{
 
     private static int counter = 1;
 
@@ -10,10 +10,11 @@ public class Payment {
     private int feeId;
     private String residentId;
     private double amount;
-    private String mode;          // simple string instead of enum
+    private String mode;
     private LocalDateTime timestamp;
 
-    public Payment(int feeId, String residentId, double amount, String mode) {
+    public Payment(int feeId, String residentId, double amount, String mode) 
+    {
         this.paymentId = counter++;
         this.feeId = feeId;
         this.residentId = residentId;
@@ -22,20 +23,38 @@ public class Payment {
         this.timestamp = LocalDateTime.now();
     }
 
-    public int getPaymentId() { return paymentId; }
-    public int getFeeId() { return feeId; }
-    public String getResidentId() { return residentId; }
-    public double getAmount() { return amount; }
-    public String getMode() { return mode; }
-    public LocalDateTime getTimestamp() { return timestamp; }
+    public int getPaymentId() 
+    { 
+        return paymentId;
+    }
 
-    @Override
-    public String toString() {
-        return "[Txn#" + paymentId +
-                "] FeeID: " + feeId +
-                " | " + residentId +
-                " | ₹" + amount +
-                " via " + mode +
-                " | " + timestamp;
+    public int getFeeId() 
+    { 
+        return feeId;
+    }
+
+    public String getResidentId() 
+    { 
+        return residentId;
+    }
+
+    public double getAmount() 
+    { 
+        return amount;
+    }
+
+    public String getMode()
+    { 
+        return mode;
+    }
+    
+    public LocalDateTime getTimestamp() 
+    { 
+        return timestamp; 
+    }
+
+    public String toString() 
+    {
+        return "Transaction ID: " + paymentId + " | FeeID: " + feeId + " | " + residentId + " | ₹" + amount + " via " + mode + " | " + timestamp;
     }
 }
